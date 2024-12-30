2c1dbf23e5ceb67d79c5751a14c97e6995f228ea
Edward Shao <edwardshao@csie.io>
2024-12-30 16:42:31 +0800

    add manifest.json and icons for PWA

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,17 @@ const CACHE_CONFIG = {
         '/css/styles.css',
         '/js/cadence.js',
         '/js/main.js',
-        '/img/runner.png'
+        '/img/runner.png',
+        '/icons/icon-48x48.png',
+        '/icons/icon-72x72.png',
+        '/icons/icon-96x96.png',
+        '/icons/icon-128x128.png',
+        '/icons/icon-144x144.png',
+        '/icons/icon-152x152.png',
+        '/icons/icon-192x192.png',
+        '/icons/icon-256x256.png',
+        '/icons/icon-384x384.png',
+        '/icons/icon-512x512.png'
     ]
 };
 
